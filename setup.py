3bb4ee52c8626126c148dfb3fcb80fec4197d14b
Guilherme Henrique <guihss.cs@gmail.com>
2022-02-27 15:32:01 -0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ont',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/guihcs/omatch',
+    license='',
+    author='guilherme',
+    author_email='guihss.cs@gmail.com',
+    description='Ontology Matching Utilities'
+)
