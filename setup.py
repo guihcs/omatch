3b446932f2f68a4aeff91f04592613284e705630
Guilherme Henrique <guihss.cs@gmail.com>
2022-02-27 15:40:51 -0300

    added requires

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import setup
 setup(
     name='ont',
     version='0.1',
-    packages=['rdflib', 'pandas', 'numpy'],
+    packages=[''],
     url='https://github.com/guihcs/omatch',
     license='',
     author='guilherme',
     author_email='guihss.cs@gmail.com',
-    description='Ontology Matching Utilities'
+    description='Ontology Matching Utilities',
+    requires=['rdflib', 'pandas', 'numpy', 'multiprocessing_on_dill']
 )
